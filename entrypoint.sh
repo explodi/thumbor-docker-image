f684157bdb14a98ff2c0725374264660e2a86bdf
cristian <phaser@tubatuba.net>
2020-06-29 13:28:55 -0400

    build

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 rm -rf /thumbor.conf
 thumbor-config > /thumbor.conf
-if [ -n "$THUMBOR_SECURITY_KEY" ];
-	then echo -e "\nSECURITY_KEY='$THUMBOR_SECURITY_KEY'" >> /thumbor.conf && echo -e "\nALLOW_UNSAFE_URL=False"  >> /thumbor.conf
-fi;
+echo -e "\nSECURITY_KEY='$THUMBOR_SECURITY_KEY'" >> /thumbor.conf
+echo -e "\nALLOW_UNSAFE_URL=False" >> /thumbor.conf
 echo -e "\nOPTIMIZERS = ['thumbor.optimizers.jpegtran']" >> /thumbor.conf
 thumbor -c /thumbor.conf -p 8888
