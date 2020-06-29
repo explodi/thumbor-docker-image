9b26b0b217a1eb4e0640b3fd158cdd1e7ce6bfeb
cristian <phaser@tubatuba.net>
2020-06-29 13:57:11 -0400

    cat

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,5 @@ thumbor-config > /thumbor.conf
 echo -e "\nSECURITY_KEY='$THUMBOR_SECURITY_KEY'" >> /thumbor.conf
 echo -e "\nALLOW_UNSAFE_URL=False" >> /thumbor.conf
 echo -e "\nOPTIMIZERS = ['thumbor.optimizers.jpegtran']" >> /thumbor.conf
+cat /thumbor.conf
 thumbor -c /thumbor.conf -p 8888
